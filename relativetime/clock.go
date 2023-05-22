7d68c96d4f073127fb45cb2095f06fcf60b5f28c
noodlebox <ash@noodlebox.moe>
2023-05-22 17:14:41 -0400

    Implement most of the logic for relativetime.Clock

--- relativetime/clock.go
+++ relativetime/clock.go
@@ -1,6 +1,6 @@
 package relativetime
 
-// The minimal API needed to serve as a reference for a RelativeClock
+// The minimal API needed to serve as a reference clock
 // For example, `realtime.Clock` satisfies:
 //   `RClock[time.Time, time.Duration, *realtime.Timer]`
 type RClock[T Time[T, D], D Duration, TM RTimer[T, D]] interface {
@@ -26,6 +26,7 @@ type Time[T any, D Duration] interface {
 	Sub(T) D
 	After(T) bool
 	Before(T) bool
+	Equal(T) bool
 }
 
 // Minimal API needed for a Duration implementation
@@ -35,15 +36,195 @@ type Duration interface {
 }
 
 type Clock[T Time[T, D], D Duration, RT RTimer[T, D]] struct {
-	Ref RClock[T, D, RT]
+	ref       RClock[T, D, RT]
+	scale     float64
+	active    bool
+	now, rNow T // last sync point
+
+	sched queue[T, D] // Upcoming events, in local time
+	waker RTimer[T, D]
+	sleep chan struct{}
 }
 
-func NewClock[T Time[T, D], D Duration, RT RTimer[T, D]](ref RClock[T, D, RT]) *Clock[T, D, RT] {
-	return &Clock[T, D, RT]{ref}
+func NewClock[T Time[T, D], D Duration, RT RTimer[T, D]](ref RClock[T, D, RT], at T, scale float64) (c *Clock[T, D, RT]) {
+	c = &Clock[T, D, RT]{
+		ref:    ref,
+		active: false,
+		scale:  scale,
+		now:    ref.Now(),
+		rNow:   at,
+	}
+	return
+}
+
+// Syncing with the reference clock is done lazily. This method updates the
+// sync points based on difference between a new reference time and the last
+// sync point. Fields that would affect how the reference is tracked should
+// not change between resyncs. This should be called to ensure sync points
+// are not stale before any change to one of these fields.
+func (c *Clock[T, D, RT]) advanceRef(now T) {
+	then := c.rNow
+
+	// Update ref sync time
+	c.rNow = now
+
+	// No local change if stopped, scale is zero, or ref clock hasn't changed
+	if !c.active || c.scale == 0.0 || now.Equal(then) {
+		return
+	}
+	dt := now.Sub(then)
+	if c.scale != 1.0 {
+		// Apply scale via conversion to float64 in seconds
+		dt = c.ref.Seconds(dt.Seconds() * c.scale)
+	}
+	// We're at now now.
+	c.now = c.now.Add(dt)
+}
+
+func (c *Clock[T, D, RT]) stopWaker() {
+	if c.waker == nil {
+		return
+	}
+
+	// Interrupt waker routine if still running
+	select {
+	case _, ok := <-c.sleep:
+		if !ok {
+			// Already ended (c.sleep closed)
+			return
+		}
+		// Did not consume from timer channel
+		if !c.waker.Stop() {
+			// Clear channel if timer has triggered but waker routine hadn't
+			// consumed it before being interrupted
+			<-c.waker.C()
+		}
+	}
+}
+
+func (c *Clock[T, D, RT]) resetWaker() {
+	if !c.active || c.scale == 0.0 {
+		return
+	}
+
+	next := c.sched.peek()
+	if next == nil {
+		// Nothing current scheduled
+		return
+	}
+	// Duration on reference clock until next timer should trigger
+	dt := c.ref.Seconds(next.when.Sub(c.now).Seconds() / c.scale)
+
+	if c.waker == nil {
+		c.waker = c.ref.NewTimer(dt)
+	} else {
+		c.waker.Reset(dt)
+	}
+
+	sleep := make(chan struct{})
+	go func() {
+		select {
+		case t := <-c.waker.C():
+			// Advance clock to t and process any timers that should trigger
+			c.wake(t)
+		case sleep <- struct{}{}:
+			// Interrupted
+		}
+		// Signal that we've finished
+		close(sleep)
+	}()
+	c.sleep = sleep
+}
+
+// Check schedule for pending events that should trigger now.
+func (c *Clock[T, D, RT]) checkSchedule() {
+	c.stopWaker()
+
+	for t := c.sched.peek(); t != nil && t.when.Before(c.now); t = c.sched.peek() {
+		if t.period.Seconds() <= 0 {
+			c.sched.unschedule(t)
+		} else {
+			t.when = c.now.Add(t.period)
+			c.sched.reschedule(t)
+		}
+		t.f(c.now)
+	}
+
+	c.resetWaker()
+}
+
+// This method is called whenever a reference timer triggers.
+func (c *Clock[T, D, RT]) wake(now T) {
+	// Don't step backwards if this callback ends up delayed
+	if now.After(c.rNow) {
+		c.advanceRef(now)
+	}
+
+	c.checkSchedule()
+}
+
+// Start() begins tracking the reference clock, if not already running.
+// It is fine to call Start() on a clock that is already running.
+func (c *Clock[T, D, RT]) Start() {
+	// Sync up first
+	c.advanceRef(c.ref.Now())
+
+	c.active = true
+}
+
+// Stop() stops tracking the reference clock, if currently running.
+// It is fine to call Stop() on a clock that is not running.
+func (c *Clock[T, D, RT]) Stop() {
+	// Sync up first
+	c.advanceRef(c.ref.Now())
+
+	c.active = false
+}
+
+func (c *Clock[T, D, RT]) Active() bool {
+	return c.active
+}
+
+func (c *Clock[T, D, RT]) SetScale(scale float64) {
+	// Sync up first
+	c.advanceRef(c.ref.Now())
+
+	c.scale = scale
+}
+
+func (c *Clock[T, D, RT]) Scale() float64 {
+	return c.scale
+}
+
+// Set the local sync point with the current reference time to `now`
+// If any timers are active, a value of `now` earlier than the previous
+// setting may lead to undefined behavior.
+func (c *Clock[T, D, RT]) Set(now T) {
+	// Reset sync point to given time
+	c.now, c.rNow = now, c.ref.Now()
+
+	// Check whether we're due for any scheduled events
+	c.checkSchedule()
+}
+
+// Advance the local time forward by `dt`.
+// If any timers are active, a negative value for dt may lead to undefined
+// behavior.
+func (c *Clock[T, D, RT]) Step(dt D) {
+	// Sync up first
+	c.advanceRef(c.ref.Now())
+
+	c.now = c.now.Add(dt)
+
+	// Check whether we're due for any scheduled events
+	c.checkSchedule()
 }
 
 func (c *Clock[T, D, RT]) Now() T {
-	return c.Ref.Now()
+	// Sync up
+	c.advanceRef(c.ref.Now())
+
+	return c.now
 }
 
 func (c *Clock[T, D, RT]) Since(t T) D {
---
+++ relativetime/schedule.go
@@ -0,0 +1,72 @@
+package relativetime
+
+import (
+	"container/heap"
+)
+
+type timer[T Time[T, D], D Duration] struct {
+	f      func(T)
+	when   T
+	period D
+	index  int
+}
+
+type queue[T Time[T, D], D Duration] []*timer[T, D]
+
+// Implement sort.Interface
+func (q *queue[T, D]) Len() int {
+	return len(*q)
+}
+
+func (q *queue[T, D]) Less(i, j int) bool {
+	return (*q)[i].when.Before((*q)[j].when)
+}
+
+func (q *queue[T, D]) Swap(i, j int) {
+	(*q)[i], (*q)[j] = (*q)[j], (*q)[i]
+	(*q)[i].index, (*q)[j].index = i, j
+}
+
+// Implement container.heap.Interface
+func (q *queue[T, D]) Push(x any) {
+	t := x.(*timer[T, D])
+	t.index = len(*q)
+	*q = append(*q, t)
+}
+
+func (q *queue[T, D]) Pop() any {
+	n := len(*q) - 1
+	t := (*q)[n]
+	(*q)[n] = nil
+	t.index = -1
+	*q = (*q)[:n]
+	return t
+}
+
+func (q *queue[T, D]) peek() *timer[T, D] {
+	if len(*q) == 0 {
+		return nil
+	}
+	return (*q)[0]
+}
+
+func (q *queue[T, D]) schedule(t *timer[T, D]) *timer[T, D] {
+	heap.Push(q, t)
+	return t
+}
+
+func (q *queue[T, D]) unschedule(t *timer[T, D]) *timer[T, D] {
+	if t.index == -1 {
+		return t
+	}
+	heap.Remove(q, t.index)
+	return t
+}
+
+func (q *queue[T, D]) reschedule(t *timer[T, D]) *timer[T, D] {
+	if t.index == -1 {
+		return q.schedule(t)
+	}
+	heap.Fix(q, t.index)
+	return t
+}
