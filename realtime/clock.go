71ccb3f4c76c1a01fccef4fd1ccbee339368e8b1
noodlebox <ash@noodlebox.moe>
2023-05-24 17:50:58 -0400

    Add references to all constants in time package

--- realtime/clock.go
+++ realtime/clock.go
@@ -4,12 +4,13 @@ import (
 	"time"
 )
 
+type Time = time.Time
+type Duration = time.Duration
 type Location = time.Location
 type Month = time.Month
 type Weekday = time.Weekday
-type Duration = time.Duration
-type Time = time.Time
 
+// Duration constants
 const (
 	Nanosecond  = time.Nanosecond
 	Microsecond = time.Microsecond
@@ -17,6 +18,33 @@ const (
 	Second      = time.Second
 	Minute      = time.Minute
 	Hour        = time.Hour
+)
+
+// Month constants
+const (
+	January   = time.January
+	February  = time.February
+	March     = time.March
+	April     = time.April
+	May       = time.May
+	June      = time.June
+	July      = time.July
+	August    = time.August
+	September = time.September
+	October   = time.October
+	November  = time.November
+	December  = time.December
+)
+
+// Weekday constants
+const (
+	Sunday    = time.Sunday
+	Monday    = time.Monday
+	Tuesday   = time.Tuesday
+	Wednesday = time.Wednesday
+	Thursday  = time.Thursday
+	Friday    = time.Friday
+	Saturday  = time.Saturday
 )
 
 // Layouts
